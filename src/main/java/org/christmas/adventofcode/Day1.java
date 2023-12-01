aadb04a64d783cba5ef34e45af95bc9bf50b1c33
sophiealice.lyon <sophiealice.lyon@ballys.com>
2023-12-01 10:49:52 +0000

    main Add set up for advent of code

---
+++ src/main/java/org/christmas/adventofcode/Day1.java
@@ -0,0 +1,7 @@
+package org.christmas.adventofcode;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Day1 {
+}
---
+++ src/test/java/org/christmas/adventofcode/Day1Test.java
@@ -0,0 +1,22 @@
+package org.christmas.adventofcode;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class Day1Test {
+
+    @Test
+    void day1_test() {
+        // given
+
+        // when
+
+        // then
+        assertThat(true).isTrue();
+    }
+
+}
